f1e3b1697b59b3ca648b065ac270f99d8cbe37b1
Xit Tian <985186708@qq.com>
2019-07-01 10:42:57 +0800

    自己递归

--- 1.php
+++ 1.php
@@ -1,10 +1,26 @@
 <?php 
 
-echo 'hello,world';
+function rm_dir($path) {
+	if(!is_dir($path)){
+		return NULL;
+	}
 
-echo 'I love you ';
-echo 'I learn github';
+	$dh = opendir($path);
+	while($row=readdir($dh)!==false) {
+		if($row=='.'||$row=='..') {
+			continue;
+		}
+		if(!is_dir($path.'/'.$row)) {
+			unlink($path.'/'.$row);
+		}else{
 
-echo 'happy github';
-
+			rm_dir($path.'/'.$row);
+		}
+	}
+	closedir($dh);
+	rmdir($path);
+	echo '删除了',$path,'文件夹','<br />';
+	return true;
+	
+}
  ?>
